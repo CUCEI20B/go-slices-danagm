4750f40e44e0572f762cec9233f2953cbc4ebf01
danagm <dana.gomez@alumnos.udg.mx>
2020-10-11 14:40:33 -0500

    slices hecho prueba 1

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	var curr int
+	var total int = 0
+	fmt.Scanln(&n)
+	slice := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanln(&curr)
+		slice = append(slice, curr)
+	}
+	for i := 0; i < len(slice); i++ {
+		total += slice[i]
+	}
+	fmt.Println(total)
 }
