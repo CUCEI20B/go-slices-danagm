9629b8a1825f2f4592438847e9938b80f5ff5183
danagm <dana.gomez@alumnos.udg.mx>
2020-10-15 21:00:41 -0500

    prueba slices

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var n int
 	var curr int
-	var total int = 0
+	var tot int = 0
 	fmt.Scanln(&n)
 	slice := make([]int, 0, n)
 	for i := 0; i < n; i++ {
@@ -13,7 +13,7 @@ func main() {
 		slice = append(slice, curr)
 	}
 	for i := 0; i < len(slice); i++ {
-		total += slice[i]
+		tot += slice[i]
 	}
-	fmt.Println(total)
+	fmt.Println(tot)
 }
